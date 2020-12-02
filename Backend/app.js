d3f800ec258cb8750c4a2b0b9f2b5cf01f7ff8b9
Abdullah Azzam <57374597+pemeluksenja@users.noreply.github.com>
2020-12-02 09:02:51 +0700

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-const handlebars = require("express-handlebars");
 const connectDB = require("./config/connectDB");
 const path = require("path");
 const passport = require("passport");
