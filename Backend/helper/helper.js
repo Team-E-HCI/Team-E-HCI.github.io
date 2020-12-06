f92fe533a3c37aa5e6cd47513bb05a74258f5a82
Abdullah Azzam <57374597+pemeluksenja@users.noreply.github.com>
2020-12-06 20:15:15 +0700

    Update helper.js

--- Backend/helper/helper.js
+++ Backend/helper/helper.js
@@ -4,16 +4,16 @@ const _ = require("underscore");
 const Q = require("q");
 
 function upload(file) {
+  //replace cloud_name, api_key & api_secret with your own cloudinary acc
   cloudinary.config({
-    cloud_name: "pemeluksenja",
-    api_key: "737684478349774",
-    api_secret: "P2dN2hBKxi65_jwO6_sjNI2ugf0",
+    cloud_name: "xxx",
+    api_key: "xxx",
+    api_secret: "xxx",
   });
 
   return new Q.Promise((resolve, reject) => {
     cloudinary.v2.uploader.upload(
       file,
-      { width: 50, height: 50 },
       (err, res) => {
         if (err) {
           console.log("Cloudinary error", err);
