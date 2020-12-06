ae18c827e7855b0c050bf2aba5db2000ac3e45b3
Abdullah Azzam <57374597+pemeluksenja@users.noreply.github.com>
2020-12-06 17:23:31 +0700

    Update User.js

--- Backend/models/User.js
+++ Backend/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcryptjs');
 const Akun = new mongoose.Schema({
-  name: {
+  nama: {
     type: String,
     required: true,
     max: 255,
@@ -23,5 +24,19 @@ const Akun = new mongoose.Schema({
     default: Date.now,
   },
 });
+
+Akun.methods.matchPassword = async function (enteredPassword){
+  return await bcrypt.compare(enteredPassword, this.password);
+}
+
+Akun.pre('save', async function (next){
+  if (!this.isModified('password')){
+    next()
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+})
+
 
 module.exports = mongoose.model("Akun", Akun);
