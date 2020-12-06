f56311a80d1ba1bde66708175c9ee0d1ed1a5ae2
Abdullah Azzam <57374597+pemeluksenja@users.noreply.github.com>
2020-12-06 17:22:37 +0700

    menambahkan skema untuk postingan

---
+++ Backend/models/Post.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const komentarSchema = mongoose.Schema({
+  pengguna: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Akun",
+  },
+  komen: {
+    type: String,
+    required: true,
+  },
+  tanggalKomentar: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const postSchema = mongoose.Schema({
+  pengguna: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Akun",
+  },
+  judul: {
+    type: String,
+    required: true,
+  },
+  postingan: {
+    type: String,
+    required: true,
+  },
+  gambar: {
+    type: [],
+  },
+  komentar: [komentarSchema],
+  kategori: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Konten", postSchema, "Konten");
