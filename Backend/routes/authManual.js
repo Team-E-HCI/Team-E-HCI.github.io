a99c923ed41ef84bbb38015e445fb67c15b7112b
Abdullah Azzam <57374597+pemeluksenja@users.noreply.github.com>
2020-12-13 14:07:24 +0700

    Update authManual.js

--- Backend/routes/authManual.js
+++ Backend/routes/authManual.js
@@ -1,12 +1,16 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 const {
   authManual,
   registerManual,
-} = require('../controllers/controllerPengguna')
-const { pelindung } = require('../middleware/validation')
+  profilPengguna,
+  updateProfil,
+} = require("../controllers/controllerPengguna");
+const { pelindung } = require("../middleware/validation");
 
 router.route("/register/manual").post(registerManual).get(pelindung);
 router.post("/login/manual", authManual);
+router.route("/profile").get(pelindung, profilPengguna);
+router.route("/profile/update").post(pelindung, updateProfil);
 
-module.exports = router
+module.exports = router;
