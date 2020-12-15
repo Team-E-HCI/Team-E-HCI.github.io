a053958535339edd12304943216378faaafa4f14
reezyx <rudiansyahpratama04@gmail.com>
2020-12-15 21:37:57 +0700

    modification and add a file

--- frontend/src/Components/Ajukan.jsx
+++ frontend/src/Components/Ajukan.jsx
@@ -1,23 +1,41 @@
 import "../App.css";
-import { Nav, NavLink, NavItem, Button, FormControl, InputGroup, Dropdown } from "react-bootstrap";
+import React from 'react'
+import {Form, Button, FormControl, InputGroup, Dropdown } from "react-bootstrap";
 import ajukan from "../Assets/Pertanyaan.png";
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import { useState } from "react";
+import {FiUpload} from 'react-icons/fi'
 
-const Ajukan = ({title, items, multiSelect = false}) => {
+const Ajukan = ({onEditorStateChange, defaultValue},props) => {
   const [value,setValue]=useState('');
+
+  const onChange = (event, editor ) => {
+      const data = editor.getData();
+      console.log(  data  );
+      onEditorStateChange(data)
+    };
+
   const handleSelect=(e)=>{
     console.log(e);
     setValue(e)
   }
 
+  const hiddenFileInput = React.useRef(null);
+  
+  const handleClick = event => {
+    hiddenFileInput.current.click();
+  };  const handleChange = event => {
+    const fileUploaded = event.target.files[0];
+    props.handleFile(fileUploaded);
+  };
+  
   return (
     <>
     <div className='container-fluid p-0'>
       <div className='row'>
           <div className='col-sm-6 offset-1'>
-            <h1 className='mt-5 ajukan-content'>
+            <h1 className='mt-3 ajukan-content'>
               Ajukan Pertanyaan
             </h1>
             <p className='mt-4 text-ajukan'>Judul</p>
@@ -38,12 +56,28 @@ const Ajukan = ({title, items, multiSelect = false}) => {
               </Dropdown>
             <p className='mt-3 text-ajukan'>Konten</p>
             <InputGroup>
-              <CKEditor editor={ClassicEditor} style={{height: '200px'}} onInit={editor =>{}}/>
+              <CKEditor 
+              editor={ClassicEditor}
+              onInit={editor =>{}}
+              config={{removePlugins: ['Image', 'ImageCaption','Table', 'ImageStyle', 'ImageToolbar', 'ImageUpload', 'MediaEmbed']}}
+              data={defaultValue}
+              />
             </InputGroup>
-            <Button size='md'className='p-2 pl-5 pr-5 mt-4 btn-ajukan'>Ajukan</Button>
+            <Form>
+              <Button className='mt-3' onClick={handleClick}>
+              <FiUpload className='mb-1 mr-2'/>Upload Gambar/Video
+              </Button>
+              <Form.Group>
+                <Form.File id="exampleFormControlFile1" label="Example file input" 
+                ref={hiddenFileInput}
+                onChange={handleChange}
+                style={{display:'none'}} />
+              </Form.Group>
+            </Form>
+            <Button size='md'className='p-2 pl-5 pr-5 mt-1 input-ajukan'>Ajukan</Button>
           </div>
-        <div className='col-md-4 p-5 ml-4'>
-          <img src={ajukan} alt='Home-Ilustration' className='img-fluid img-content' width='75%'/>
+        <div className='col-md-4 ml-5 my-auto'>
+          <img src={ajukan} alt='Home-Ilustration' className='img-fluid img-content mx-auto' style={{display: "flex"}} width='60%'/>
         </div>
       </div>
     </div>
---
+++ frontend/src/Components/Profile.jsx
@@ -0,0 +1,97 @@
+import "../App.css";
+import profile from '../Assets/hanif.jpeg'
+import { Form, Col, Row, ListGroup, Image } from "react-bootstrap";
+import {FaEdit} from 'react-icons/fa'
+import {RiDeleteBin6Line} from 'react-icons/ri'
+
+const Profile = () => {
+  return (
+    <>
+    <div className='container-fluid p-0'>
+      <div className='row ml-1 mr-1 mt-3'>
+          <div className="col-sm-4 my-auto mx-auto">
+            <Image src={profile} className="mx-auto" style={{display: "flex"}} width="60%" roundedCircle/>
+          </div>
+          <div className="col-sm-5">
+            <div style={{display:"flex"}}>
+              <h1 className="profile-header mt-3">Profil</h1>
+              <a className="mt-4 ml-auto edit-profile" href="#"><FaEdit/></a>
+            </div>
+            <Form.Group as={Row} className="profile-content" controlId="formPlaintextName">
+            <Form.Label column className="col-sm-4">
+              Nama Lengkap
+            </Form.Label>
+            <Col className="col-sm-6">
+            <Form.Control plaintext readOnly defaultValue="mamankhehe" />
+            </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="profile-content" controlId="formPlaintextName">
+            <Form.Label column className="col-sm-4">
+              Email
+            </Form.Label>
+            <Col className="col-sm-6">
+            <Form.Control plaintext readOnly defaultValue="mamankhehe@gmail.com" />
+            </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="profile-content" controlId="formPlaintextName">
+            <Form.Label column className="col-sm-4">
+              Github
+            </Form.Label>
+            <Col className="col-sm-6">
+            <Form.Control plaintext readOnly defaultValue="mamankjaprut" />
+            </Col>
+            </Form.Group>
+
+            <Form.Group as={Row} className="profile-content" controlId="formPlaintextName">
+            <Form.Label column className="col-sm-4">
+              Twitter
+            </Form.Label>
+            <Col className="col-sm-6">
+            <Form.Control plaintext readOnly defaultValue="mamankdarno"/>
+            </Col>
+            </Form.Group>
+          </div>
+          <div className="col-sm-3"></div>
+          <div className="col-sm-12">
+            <h1 className="profile-header mt-5">Riwayat Pertanyaan</h1>
+            <ListGroup className="mt-5 mb-5">
+              <div className="question-profile" style={{display: "flex"}}>
+                <ListGroup.Item variant="primary" className="mt-2 w-100 mr-3">AAN AYAM</ListGroup.Item>
+                <a className="mt-3 ml-auto edit-profile mr-2" href="#"><FaEdit/></a>
+                <a className="mt-3 edit-profile" href="#"><RiDeleteBin6Line/></a>
+              </div>
+              
+              <div className="question-profile" style={{display: "flex"}}>
+                <ListGroup.Item variant="primary" className="mt-2 w-100 mr-3">LOPE NIA</ListGroup.Item>
+                <a className="mt-3 ml-auto edit-profile mr-2" href="#"><FaEdit/></a>
+                <a className="mt-3 edit-profile" href="#"><RiDeleteBin6Line/></a>
+              </div>
+
+              <div className="question-profile" style={{display: "flex"}}>
+                <ListGroup.Item variant="primary" className="mt-2 w-100 mr-3">CELALU CAYANK</ListGroup.Item>
+                <a className="mt-3 ml-auto edit-profile mr-2" href="#"><FaEdit/></a>
+                <a className="mt-3 edit-profile" href="#"><RiDeleteBin6Line/></a>
+              </div>
+
+              <div className="question-profile" style={{display: "flex"}}>
+                <ListGroup.Item variant="primary" className="mt-2 w-100 mr-3">POLEPEL</ListGroup.Item>
+                <a className="mt-3 ml-auto edit-profile mr-2" href="#"><FaEdit/></a>
+                <a className="mt-3 edit-profile" href="#"><RiDeleteBin6Line/></a>
+              </div>
+
+              <div className="question-profile" style={{display: "flex"}}>
+                <ListGroup.Item variant="primary" className="mt-2 w-100 mr-3">CAMPE MATI</ListGroup.Item>
+                <a className="mt-3 ml-auto edit-profile mr-2" href="#"><FaEdit/></a>
+                <a className="mt-3 edit-profile" href="#"><RiDeleteBin6Line/></a>
+              </div>
+            </ListGroup>
+          </div>
+        </div>
+    </div>
+    </>
+  );
+};
+
+export default Profile;
