14de4e968f28a265a7e67f6704b5d1557d4d386b
Abdullah Azzam <57374597+pemeluksenja@users.noreply.github.com>
2020-12-17 21:42:44 +0700

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,84 +1,65 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const morgan = require('morgan')
-const handlebars = require('express-handlebars')
-const connectDB = require('./config/connectDB')
-const path = require('path')
-const passport = require('passport')
-const session = require('express-session')
-const MongoStore = require('connect-mongo')(session)
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
+const connectDB = require("./config/connectDB");
+const path = require("path");
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const cookieParser = require("cookie-parser");
 
 //load config
-dotenv.config()
+dotenv.config({ path: "./config/config.env" });
 
 //passport config
-require('./config/passportGoogle')(passport)
+require("./config/passportGoogle")(passport);
 
 //connect Mongo
-connectDB()
+connectDB();
 
 //assigned express into app
-const app = express()
+const app = express();
 //body parser
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 //logging
-app.use(morgan('dev'))
+app.use(morgan("dev"));
 
 //Express Handlebars = buat nampilin hasil render
 app.engine(
-  '.hbs',
+  ".hbs",
   handlebars({
-    defaultLayout: 'main',
-    extname: '.hbs',
+    defaultLayout: "main",
+    extname: ".hbs",
   })
-)
-app.set('view engine', '.hbs')
-
-//Express Handlebars = buat nampilin hasil render
-app.engine(
-  '.hbs',
-  handlebars({
-    defaultLayout: 'main',
-    extname: '.hbs',
-  })
-)
-app.set('view engine', '.hbs')
+);
+app.set("view engine", ".hbs");
 
 //sessions
 app.use(
   session({
-    secret: 'keyboard cat',
+    secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
     store: new MongoStore({ mongooseConnection: mongoose.connection }),
   })
-)
+);
 
 //passport middleware initialized and session
-app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.initialize());
+app.use(passport.session());
 
 //static foler
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, "public")));
 
 //routes
-app.use('/', require('./routes/index'))
-app.use('/auth', require('./routes/auth'))
-app.use('/api/user', require('./routes/authManual'))
-app.use('/api/konten', require('./routes/konten'))
-
-//Custom Error handler
-app.use((err, req, res, next) => {
-  const statusCode = 200 ? 500 : req.statusCode
-  res.status(statusCode)
-  res.json({
-    message: err.message,
-  })
-})
-
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
+app.use("/api/user", require("./routes/authManual"));
+app.use("/api/konten", require("./routes/konten"));
+app.use("/api/kuesioner", require("./routes/kuesioner"));
 //PORT
-const PORT = process.env.PORT || 5000
-app.listen(PORT, console.log(`Server running on port ${PORT} `))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server running on port ${PORT} `));
